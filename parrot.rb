3274e2f4bb050f8c5951a6fc4b702b744581a5e9
Anna Dybas <a.m.dybas@gmail.com>
2019-09-02 20:44:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(talking = "Squawk")
+def parrot(talking = "Squawk!")
   puts talking
   return talking
 end
